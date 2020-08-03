6e1430080dc6a7b8ef934775f72c2da4141466ac
Prateek Gurnani <prateekgurnani10@gmail.com>
2020-08-03 11:52:43 +0530

    Data 01

---
+++ Datatypes.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Datatypes{
+public static void main(String[] args) {
+int i =4;
+double d =4.0;
+String s = "HackerRank";
+
+Scanner scan = new Scanner(System.in);
+
+int myInt;
+double myDouble;
+String myString;
+
+myInt = scan.nextInt();
+myDouble = scan.nextDouble();
+scan.nextLine();
+}
+}
